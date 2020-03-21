c129fdc88bc28988f302d4790668729ce08cd232
Metin Senturk <metinsenturk@me.com>
2020-03-20 20:30:16 -0400

    class 20-03-20 materials added

---
+++ class-2020-03-20/app.py
@@ -0,0 +1,8 @@
+import my_module
+
+# accessing to the function in a module
+my_module.greeting('Metin')
+
+# accessing an object (dict) in a module
+p = my_module.person1
+print(p)
---
+++ class-2020-03-20/my_module.py
@@ -0,0 +1,7 @@
+# dictionary
+person1 = dict(name='Mike', lastname='Olsen', age=25)
+
+
+# this is just a func
+def greeting(name):
+    return 'Hello, {}!'.format(name)
